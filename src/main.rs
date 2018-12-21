07990372752f0e38eaf784b240421b455ece2ac3
Asbjoern Clemmensen <ac@siteimprove.com>
2018-12-21 02:15:53 +0100

    Work on streaming over websockets

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use phoenix::PhoenixEvent;
 use std::fmt::Display;
 use std::error::Error;
 use serde_derive::{Serialize, Deserialize};
 use reqwest::{StatusCode};
-use std::io::{self, Read};
+use std::io::{self, Read, Write};
 use std::path::Path;
-use atty::Stream;
+use std::collections::HashMap;
+use atty::Stream as AStream;
 use clap::{Arg, App, SubCommand};
 use term_size as ts;
+
+use phoenix::{Phoenix, Event};
+use websocket::futures::sync::mpsc::channel;
+use tokio_core::reactor::Core;
+use futures::stream::Stream;
 
 fn main() {
     let matches = App::new("Copypasta")
@@ -21,13 +28,26 @@ fn main() {
             .takes_value(true))
         .subcommand(SubCommand::with_name("list")
             .about("Lists your pasta"))
+        .subcommand(SubCommand::with_name("produce"))
+        .subcommand(SubCommand::with_name("consume"))
         .get_matches();
+
+    if let Some(_) = matches.subcommand_matches("produce") {
+        produce();
+    }
+
+    if let Some(_) = matches.subcommand_matches("consume") {
+        consume();
+    }
+
+    return;
+
 
     let config_file = matches.value_of("config").unwrap_or(".pastaconfig");
 
     match get_app(config_file.to_string()) {
         Ok(app) => {
-            if atty::is(Stream::Stdin) {
+            if atty::is(AStream::Stdin) {
                 if let Some(_) = matches.subcommand_matches("list") {
                     let lst = app.list().unwrap();
                     let w = ts::dimensions()
@@ -55,6 +75,140 @@ fn main() {
         },
         Err(e) => eprintln!("Error: {:?}", e)
     }
+}
+
+#[derive(Copy, Clone, Debug)]
+enum ProducerState  {
+    ReadyToProduce,
+    WaitingForAck,
+    Producing,
+    Done
+}
+
+#[derive(Copy, Clone)]
+enum ConsumerState {
+    ReadyToConsume,
+    Consuming,
+    Done
+}
+
+enum StreamEvent<'a> {
+    Custom(&'a str),
+    Defined(&'a PhoenixEvent)
+}
+
+fn into_good<'a>(evt: &'a Event) -> StreamEvent {
+    match evt {
+        Event::Custom(x) => StreamEvent::Custom(x.as_ref()),
+        Event::Defined(x) => StreamEvent::Defined(x)
+    }
+}
+
+fn send_msg(chan: &std::sync::Arc<std::sync::Mutex<phoenix::chan::Channel>>, msg_type: &str, payload: &str) {
+    let mut chan = chan.lock().unwrap();
+    let body = serde_json::from_str(payload).unwrap();
+    chan.send(Event::Custom(msg_type.to_string()), &body);
+}
+
+fn send_msg2(chan: &mut phoenix::chan::Channel, msg_type: &str, payload: &str) {
+    let body = serde_json::from_str(payload).unwrap();
+    chan.send(Event::Custom(msg_type.to_string()), &body);
+}
+
+fn produce() {
+    let (sender, emitter) = channel(0);
+    let (callback, messages) = channel(0);
+
+    let mut p = HashMap::new();
+    p.insert("token", "xxx");
+
+    let mut phx = Phoenix::new_with_parameters(&sender, emitter, &callback, "ws://localhost:4000/socket", &p);
+    let chan = phx.channel("streams:x").clone();
+    {
+        let mut chan = chan.lock().unwrap();
+        chan.join();
+    }
+
+    send_msg(&chan, "producer_join", "{}");
+    let output_buffer = String::new();
+    let input_buffer = vec![0; 1_000_000];
+    let mut chan = chan.lock().unwrap();
+
+    let runner = messages.fold((ProducerState::ReadyToProduce, input_buffer, output_buffer), |(state, mut in_buf, mut out_buf), message| {
+        // eprintln!("SAD {:#?} {:?}", message, state); 
+        match (state, into_good(&message.event)) {
+            (ProducerState::ReadyToProduce, StreamEvent::Custom("bytes_requested")) => {
+                match io::stdin().read(&mut in_buf) {
+                    Ok(0) => {
+                        send_msg2(&mut chan, "done", "{}");
+                        Err(())
+                    },
+                    Ok(n) => {
+                        out_buf.clear();
+                        out_buf.push('"');
+                        base64::encode_config_buf(&in_buf[0..n], base64::STANDARD, &mut out_buf);
+                        out_buf.push('"');
+                        send_msg2(&mut chan, "bytes", &out_buf);
+                        Ok((ProducerState::ReadyToProduce, in_buf, out_buf))
+                    },
+                    Err(_) => {
+                        send_msg2(&mut chan, "done", "{\"error\": true}");
+                        Err(())
+                    }
+                }
+            },
+            _ => Ok((state, in_buf, out_buf))
+        }
+    });
+
+    let mut core = Core::new().unwrap();
+    core.run(runner).map(|_| ()).unwrap_or(());
+
+    eprintln!("Done producing");
+}
+
+fn consume() {
+    let (sender, emitter) = channel(0);
+    let (callback, messages) = channel(0);
+
+    let mut p = HashMap::new();
+    p.insert("token", "xxx");
+
+    let mut phx = Phoenix::new_with_parameters(&sender, emitter, &callback, "ws://localhost:4000/socket", &p);
+    let chan = phx.channel("streams:x").clone();
+    {
+        let mut chan = chan.lock().unwrap();
+        chan.join();
+    }
+
+    send_msg(&chan, "consumer_join", "{}");
+    send_msg(&chan, "request_bytes", "{}");
+
+    let mut chan = chan.lock().unwrap();
+
+    let runner = messages.fold(ConsumerState::Consuming, |state, message| {
+        // eprintln!("{:#?}", message);
+        match (state, into_good(&message.event)) {
+            (ConsumerState::Consuming, StreamEvent::Custom("bytes")) => {
+                // eprintln!("Got some bytes!");
+                let x = message.payload.get("data").unwrap().as_str().unwrap();
+                let decoded = base64::decode(x).unwrap();
+                io::stdout().write(&decoded).unwrap();
+                send_msg2(&mut chan, "request_bytes", "{}");
+                Ok(ConsumerState::Consuming)
+            },
+            (ConsumerState::Consuming, StreamEvent::Custom("no_more_data")) => {
+                eprintln!("Ate all the bytes");
+                Err(())
+            }
+            _ => Ok(state)
+        }
+    });
+
+    let mut core = Core::new().unwrap();
+    core.run(runner).map(|_| ()).unwrap_or(());
+
+    eprintln!("Done consuming");
 }
 
 fn read_all_input() -> Result<String, Box<Error>> {
