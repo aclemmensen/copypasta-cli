0956b1bf8fda25f99e38a898cdd4840efea14635
Asbjørn Clemmensen <ac@givetwise.dk>
2018-12-28 21:13:22 +0100

    Integrating options with previous work

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::io::{self, Read, Write};
 use std::path::Path;
 use std::collections::HashMap;
 use atty::Stream as AStream;
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, App, SubCommand, AppSettings};
 use term_size as ts;
 
 use phoenix::{Phoenix, Event};
@@ -15,67 +15,99 @@ use websocket::futures::sync::mpsc::channel;
 use tokio_core::reactor::Core;
 use futures::stream::Stream;
 
+const LOGIN_NAME: &str = "login";
+const LIST_NAME: &str = "list";
+const PRODUCE_NAME: &str = "produce";
+const CONSUME_NAME: &str = "consume";
+
+const DEFAULT_SCHEME: &str = "https";
+const DEFAULT_HOST: &str = "copypasta.gw.wepo.dk";
+const DEFAULT_CONFIG: &str = ".pastaconfig";
+
 fn main() {
     let matches = App::new("Copypasta")
         .version("1.0")
         .author("@aclemmensen")
         .about("Copies your pastas")
+        .setting(AppSettings::DeriveDisplayOrder)
         .arg(Arg::with_name("config")
             .short("c")
             .long("config")
             .value_name("FILE")
             .help("Sets a custom config file")
             .takes_value(true))
-        .subcommand(SubCommand::with_name("list")
+        .subcommand(SubCommand::with_name(LOGIN_NAME)
+            .about("Logs you into Copypasta"))
+        .subcommand(SubCommand::with_name(LIST_NAME)
             .about("Lists your pasta"))
-        .subcommand(SubCommand::with_name("produce"))
-        .subcommand(SubCommand::with_name("consume"))
+        .subcommand(SubCommand::with_name(PRODUCE_NAME)
+            .about("Produce a stream through your Copypasta account"))
+        .subcommand(SubCommand::with_name(CONSUME_NAME)
+            .about("Consume a stream created by a Copypasta user"))
         .get_matches();
 
-    if let Some(_) = matches.subcommand_matches("produce") {
-        produce();
-    }
+    let config_file = matches.value_of("config").unwrap_or(DEFAULT_CONFIG);
 
-    if let Some(_) = matches.subcommand_matches("consume") {
-        consume();
-    }
-
-    return;
-
-
-    let config_file = matches.value_of("config").unwrap_or(".pastaconfig");
-
-    match get_app(config_file.to_string()) {
+    match get_app_if_configured(&config_file) {
         Ok(app) => {
-            if atty::is(AStream::Stdin) {
-                if let Some(_) = matches.subcommand_matches("list") {
-                    let lst = app.list().unwrap();
-                    let w = ts::dimensions()
-                        .map(|(w, _)| w)
-                        .unwrap_or(80);
-                    
-                    for p in lst {
-                        let content = p.content.replace("\n", "\\n").replace("\t", " ").to_string();
-                        let w = std::cmp::min(w, content.chars().count());
-                        let w = w-6;
-                        let w = std::cmp::max(w, 0);
-                        let snippet = &content[0..w];
-                        println!("{:05} {}", p.id, snippet);
-                    }
-                } else {
-                    match app.latest() {
-                        Ok(pasta) => println!("{}", pasta.content),
-                        Err(e) => eprintln!("Error: {:?}", e)
-                    };
-                }
+            if let Some(_) = matches.subcommand_matches(PRODUCE_NAME) {
+                produce();
+            } else if let Some(_) = matches.subcommand_matches(CONSUME_NAME) {
+                consume();
+            } else if let Some(_) = matches.subcommand_matches(LIST_NAME) {
+                handle_list(&app);
+            } else if let Some(_) = matches.subcommand_matches(LOGIN_NAME) {
+                eprintln!("You are already logged");
             } else {
-                let input = read_all_input().unwrap();
-                app.post(input).unwrap();
+                handle_default(&app);
             }
         },
-        Err(e) => eprintln!("Error: {:?}", e)
+        Err(HeyError::NoConfigFound) => {
+            if let Some(_) = matches.subcommand_matches(LOGIN_NAME) {
+                match get_app(config_file.to_string()) {
+                   Ok(_app) => {
+                       eprintln!("You are now logged in");
+                    },
+                    Err(e) => {
+                        eprintln!("{:?}", e);
+                    }
+                }
+            } else {
+                eprintln!("You are not logged in. Run `copypasta login` first.");
+            }
+        },
+        Err(e) => panic!(e)
     }
 }
+
+fn handle_default(app: &PastaClient) {
+    if atty::is(AStream::Stdin) {
+        match app.latest() {
+            Ok(pasta) => println!("{}", pasta.content),
+            Err(e) => eprintln!("Error: {:?}", e)
+        };
+    } else {
+        let input = read_all_input().unwrap();
+        app.post(input).unwrap();
+    }
+}
+
+fn handle_list(app: &PastaClient) {
+    let lst = app.list().unwrap();
+    let w = ts::dimensions()
+        .map(|(w, _)| w)
+        .unwrap_or(80);
+    
+    for p in lst {
+        let content = p.content.replace("\n", "\\n").replace("\t", " ").to_string();
+        let w = std::cmp::min(w, content.chars().count());
+        let w = w-6;
+        let w = std::cmp::max(w, 0);
+        let snippet = &content[0..w];
+        println!("{:05} {}", p.id, snippet);
+    }
+}
+
 
 #[derive(Copy, Clone, Debug)]
 enum ProducerState  {
@@ -218,14 +250,8 @@ fn read_all_input() -> Result<String, Box<Error>> {
 }
 
 fn get_app(path: String) -> Result<PastaClient, HeyError> {
-    match PastaClient::from_config(&path) {
-        Ok(mut app) => {
-            if verify_login(&mut app)? {
-                // We went through the token flow again so we
-                // have to store an updated config
-                app.save_config().unwrap();
-            }
-            
+    match get_app_if_configured(&path) {
+        Ok(app) => {
             Ok(app)
         },
         Err(HeyError::NoConfigFound) => {
@@ -242,16 +268,39 @@ fn get_app(path: String) -> Result<PastaClient, HeyError> {
     }
 }
 
+fn get_app_if_configured(path: &str) -> Result<PastaClient, HeyError> {
+    let config = ClientConfig::load_from_file(path)?;
+    build_and_verify(path, config)
+}
+
+fn build_and_verify(path: &str, config: ClientConfig) -> Result<PastaClient, HeyError> {
+    let mut app = PastaClient {
+        config: Some(config),
+        client: reqwest::Client::new(),
+        config_path: path.to_string()
+    };
+
+    if verify_login(&mut app)? {
+        app.save_config().unwrap();
+        return Ok(app)
+    }
+
+    Err(HeyError::LoginError)
+}
+
 fn verify_login(app: &mut PastaClient) -> Result<bool, HeyError> {
     match app.login() {
         Ok(_) => Ok(false),
         Err(HeyError::NotLoggedIn(login_url)) => {
             let token = prompt_token(login_url).unwrap();
-            app.set_token(token.trim().to_string());
+            app.set_config(ClientConfig {
+                token: token.trim().to_string(),
+                host: "".to_string()
+            });
 
             match app.login() {
                 Ok(user) => {
-                    eprintln!("You are now logged in as {}!", user.username);
+                    eprintln!("welcome, {}!", user.username);
                     Ok(true)
                 },
                 Err(e) =>  {
@@ -273,8 +322,27 @@ fn prompt_token(login_url: String) -> Result<String, Box<Error>> {
 
 struct PastaClient {
     client: reqwest::Client,
-    token: Option<String>,
-    config_path: String
+    config: Option<ClientConfig>,
+    config_path: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct ClientConfig {
+    token: String,
+    host: String
+}
+
+impl ClientConfig {
+    fn load_from_file(path: &str) -> Result<ClientConfig, HeyError> {
+        let fspath = Path::new(path);
+        if !fspath.is_file() {
+            return Err(HeyError::NoConfigFound);
+        }
+
+        let content = std::fs::read_to_string(path).unwrap();
+        let deser: ClientConfig = serde_json::from_str(&content).unwrap();
+        Ok(deser)
+    }
 }
 
 #[derive(Debug)]
@@ -307,33 +375,27 @@ impl PastaClient {
     fn new(client: reqwest::Client, path: String) -> PastaClient {
         PastaClient {
             client,
-            token: None,
+            config: None,
             config_path: path
         }
     }
 
-    fn from_config(path: &String) -> Result<PastaClient, HeyError> {
-        let fspath = Path::new(path);
-        if !fspath.is_file() {
-            return Err(HeyError::NoConfigFound);
-        }
-
-        let content = std::fs::read_to_string(path).unwrap();
-        let deser: ClientConfig = serde_json::from_str(&content).unwrap();
+    fn from_config(path: &str) -> Result<PastaClient, HeyError> {
+        let config = ClientConfig::load_from_file(&path)?;
 
         Ok(PastaClient {
-            token: Some(deser.token),
+            config: Some(config),
             client: reqwest::Client::new(),
             config_path: path.to_string()
         })
     }
 
-    fn set_token(&mut self, token: String) -> () {
-        self.token = Some(token);
+    fn set_config(&mut self, config: ClientConfig) -> () {
+        self.config = Some(config);
     }
     
     fn login(&self) -> Result<UserInfo, HeyError> {
-        let mut resp = self.add_token(self.client.get("http://localhost:4000/api"))
+        let mut resp = self.add_token(self.client.get(&self.get_url("api")))
             .send()?;
         
         check_resp(&mut resp)?;
@@ -344,7 +406,7 @@ impl PastaClient {
     }
 
     fn latest(&self) -> Result<Pasta, HeyError> {
-        let mut resp = self.add_token(self.client.get("http://localhost:4000/api/latest"))
+        let mut resp = self.add_token(self.client.get(&self.get_url("api/latest")))
             .send()?;
         
         check_resp(&mut resp)?;
@@ -355,7 +417,7 @@ impl PastaClient {
     }
 
     fn list(&self) -> Result<Vec<Pasta>, HeyError> {
-        let mut resp = self.add_token(self.client.get("http://localhost:4000/api/list"))
+        let mut resp = self.add_token(self.client.get(&self.get_url("api/list")))
             .send()?;
         
         check_resp(&mut resp)?;
@@ -370,7 +432,7 @@ impl PastaClient {
             content
         };
 
-        let mut resp = self.add_token(self.client.post("http://localhost:4000/api/create"))
+        let mut resp = self.add_token(self.client.post(&self.get_url("api/create")))
             .json(&msg)
             .send()?;
         
@@ -378,14 +440,9 @@ impl PastaClient {
     }
 
     fn save_config(&self) -> Result<(), Box<Error>> {
-        if let Some(token) = &self.token {
-            let conf = ClientConfig {
-                token: token.to_string()
-            };
-
+        if let Some(conf) = &self.config {
             let ser = serde_json::to_string(&conf)?;
             std::fs::write(&self.config_path, &ser)?;
-
             Ok(())
         } else {
             Err(Box::new(HeyError::NoToken))
@@ -393,22 +450,24 @@ impl PastaClient {
     }
 
     fn add_token(&self, builder: reqwest::RequestBuilder) -> reqwest::RequestBuilder {
-        if let Some(token) = &self.token {
-            return builder.bearer_auth(token);
+        if let Some(config) = &self.config {
+            return builder.bearer_auth(config.token.to_string());
         }
 
         return builder;
+    }
+
+    fn get_url(&self, url: &str) -> String {
+        match self.config {
+            Some(ref c) => format!("{}://{}/{}", DEFAULT_SCHEME, c.host, url),
+            None => format!("{}://{}/{}", DEFAULT_SCHEME, DEFAULT_HOST, url)
+        }
     }
 }
 
 #[derive(Serialize)]
 struct CreatePasta {
     content: String
-}
-
-#[derive(Deserialize, Serialize)]
-struct ClientConfig {
-    token: String
 }
 
 #[derive(Deserialize, Debug)]
